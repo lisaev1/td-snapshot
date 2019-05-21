4710e1136517ce92bb2913b7ec709488fcc820bd
Leonid Isaev <leonid.isaev@jila.colorado.edu>
2019-05-21 10:16:07 -0400

    td-snapshot.sh: * Add a new function _btrfs_idname() to compute characteristics of a BTRFS   subvolume: ID by name or vice versa; * _btrfs_snapshot(): fix a bug when mounting the snapshot and code cleanups; * Add comments about _lvm_snapshot()

--- td-snapshot.sh
+++ td-snapshot.sh
@@ -84,48 +84,85 @@ _mnt2info() {
 }
 
 #
+# Determine subvol ID by its name (if $1 = id), or vice versa (if $1 = name) of
+# a BTRFS subvolume -- works only on a mounted FS!
+#
+# Input: $1 = what to return (id/name)
+#	$2 = known characteristic (id/name)
+#	$3 = FS mountpoint
+# Return: name of the subvolume
+_btrfs_idname() {
+	local t="$1" c="$2" m="$3"
+	local -i i1 i2
+	local -a a
+
+	case "$t" in
+		name | n ) # We want to find subvol name by its ID
+		       i1=1
+		       i2=-1
+		;;
+		id | i | ID ) # We want to find subvol ID by its name
+		     i1=-1
+		     i2=1
+		;;
+		* ) # For anythin else we quit
+		    echo -E "_btrfs_idname(): 1st arg must be either \"id\" or \"name\""
+		    exit 1
+	esac
+
+	while read -ra a; do
+		[[ "${a[$i1]##*/}" == "$c" ]] && break
+	done < <($xBTRFS subvolume list "$m")
+
+	echo -nE "${a[$i2]}"
+}
+
+#
 # Make & mount ro snapshot when the backup target is on btrfs
 #
 # Input: $1 = device
 # 	$2 = subvolid
+# Return: ID of the snapshot
 _btrfs_snapshot() {
-	local dev="$1" s="$2" name snap_name
-	local -a a
+	local dev="$1" subvolid="$2" name
 	local -r snap_dir="_${SALT}_backup_snapshots"
 
 	# Mount the entire BTRFS device "$dev"
 	$xMOUNT -o "${MOUNTOPTS},subvolid=5" "$dev" "$SRC_MNT"
 
-	# For our subvolid "$s" (!= 5), find the corresponding name. The
-	# subvol_id = 5 does not show up in the list and "$name" is unset.
-	if [[ "$s" != "5" ]]; then
-		while read -ra a; do
-			[[ "${a[1]}" == "$s" ]] && break
-		done < <($xBTRFS subvolume list "$SRC_MNT")
-		name="${a[-1]}"
-	fi
+	# For our subvolid (!= 5), find the corresponding name. The
+	# subvolid = 5 does not show up in the list and "$name" is unset.
+	[[ "$subvolid" != "5" ]] && \
+		name="$(_btrfs_idname "name" "$subvolid" "$SRC_MNT")"
 
 	# If necessary, make a subvolume for future snapshots
 	[[ ! -d "${SRC_MNT}/$snap_dir" ]] && \
 		$xBTRFS subvolume create "${SRC_MNT}/$snap_dir"
 
 	# Snapshot our parent subvolume and mount it ro
-	snap_name="backup-snapshot-${name:-"id5"}-$SFX"
+	subvolid="backup-snapshot-${name:-"id5"}-$SFX"
 	$xBTRFS subvolume snapshot -r "${SRC_MNT}/$name" \
-		"${SRC_MNT}/${snap_dir}/$snap_name"
+		"${SRC_MNT}/${snap_dir}/$subvolid"
 
+	# Mount the snapshot (by its ID)
+	subvolid="$(_btrfs_idname "id" "$subvolid" "$SRC_MNT")"
 	$xUMOUNT "$SRC_MNT"
-	$xMOUNT -o "ro,${MOUNTOPTS},subvol=$snap_name" "$dev" "$SRC_MNT"
+	$xMOUNT -o "ro,${MOUNTOPTS},subvolid=$subvolid" "$dev" "$SRC_MNT"
 
-	echo -nE "$name"
+	echo -nE "$subvolid"
 }
 
 #
 # LVM snapshots.
+# Contrary to btrfs, LVM needs an explicit space allocation for snapshots.
+# Therefore, we will:
+# - create a 500M loop device located in RAM (/dev/shm);
+# - turn it into a PV, and extend the proper VG over it;
+# - make a snapshot of the proper LV.
 #
+# Input: 
 _lvm_snapshot() {
 }
-
 
 #
 # Initial setup (if called for the first time)
